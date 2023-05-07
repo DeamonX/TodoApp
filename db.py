e81b918c0ea7a6d3663c2c81023400fedbdcd1fa
Tamás Klein <tamas.klein@bluebax.com>
2023-05-07 12:25:56 +0200

    added gitignore, inited main

--- db.py
+++ db.py
@@ -2,18 +2,21 @@
 import mysql.connector
 
 # connecting to the mysql server
-db = mysql.connector.connect(host="localhost", user="root", passwd="")
 
-# cursor object c
-c = db.cursor()
 
-# fetching all the databases
-c.execute("SHOW DATABASES")
+def connectSql():
+    db = mysql.connector.connect(host="localhost", user="root", passwd="")
 
-# printing all the databases
-for i in c:
-    print(i)
-c = db.cursor()
+    # cursor object c
+    c = db.cursor()
 
-# finally closing the database connection
-db.close()
+    # fetching all the databases
+    c.execute("SHOW DATABASES")
+
+    # printing all the databases
+    for i in c:
+        print(i)
+    c = db.cursor()
+
+    # finally closing the database connection
+    db.close()
