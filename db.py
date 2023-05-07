c5c0dd2c1b9ca3b5814cb0e58835f3fc20e54442
Tamás Klein <tamas.klein@bluebax.com>
2023-05-07 17:08:46 +0200

    continued db logic started tkinter layout imported custom theme.json

--- db.py
+++ db.py
@@ -1,49 +1,48 @@
 import mysql.connector
 
-db = mysql.connector.connect(host="localhost", user="root", passwd="")
-c = db.cursor()
-
+#Consts
 DB_NAME = "todoapp"
+CREATE_DATABASE = "CREATE DATABASE todoapp"
+CREATE_USERS_TABLE = "create table users (email VARCHAR(50) NOT NULL,username VARCHAR(20) NOT NULL,password VARCHAR(20) NOT NULL,PRIMARY KEY ( email ));"
+CREATE_TASKS_TABLE = "create table tasks (id INT AUTO_INCREMENT,userEmail VARCHAR(50) NOT NULL,status INT NOT NULL,title VARCHAR(100) NOT NULL,description VARCHAR(255),PRIMARY KEY ( id ),FOREIGN KEY (userEmail) REFERENCES users(email));"
+INSERT_ADMIN_USER = "INSERT INTO users (email,username,password) VALUES ('admin','admin','admin');"
+
+#Vars
+db = mysql.connector.connect(host="localhost", user="root", passwd="")
+c = db.cursor(buffered=True)
 querry = ""
 
-def initDB():
-    querry = "SHOW DATABASES LIKE %s"  
-    c.execute(querry,(DB_NAME,))
-    if c.fetchall() == [] : createDatabase()    # Check if DB 'todoapp' exits
-    else : readExistingDB()
 
-def readExistingDB():
+# Functions
+
+async def initDB():   # Init database on application start.
+    c.execute("SHOW DATABASES LIKE '"+ DB_NAME+"'")
+    if c.fetchone() == [] : await createDatabase()    # Check if DB 'todoapp' exits
+    else : db.database = DB_NAME   # Set DB to 'todoapp'
+
+def readExistingDB():   # Get data of user logged in 
     print("existing")
 
-def createDatabase():  
-    querry = "CREATE DATABASE todoapp"  # Create table because fetch returned with empty array
-    c.execute(querry)
+async def createDatabase():   # Create table because fetch returned with empty array
+    c.execute(CREATE_DATABASE)  
 
-    db.database = "todoapp" # Set DB to 'todoapp'
+    db.database = DB_NAME   # Set DB to 'todoapp'
+    
+    c.execute(CREATE_USERS_TABLE)   # Create users table
+    c.execute(CREATE_TASKS_TABLE)   # Create tasks table
+    c.execute(INSERT_ADMIN_USER)    # Insert Admin user for testing
 
-    # Create users table
-    c.execute(
-        """
-        create table users (
-            email VARCHAR(50) NOT NULL,
-            username VARCHAR(20) NOT NULL,
-            password VARCHAR(20) NOT NULL,
-            PRIMARY KEY ( email )
-        );"""
-    )
-    # Create tasks table
-    c.execute(
-        """
-        create table tasks (
-            id INT AUTO_INCREMENT,
-            userEmail VARCHAR(50) NOT NULL,
-            status INT NOT NULL,
-            title VARCHAR(100) NOT NULL,
-            description VARCHAR(255),
-            PRIMARY KEY ( id ),
-            FOREIGN KEY (userEmail) REFERENCES users(email)
-        );"""
-    )
+    db.commit()
 
 def closeConnection():  # Close connection
     db.close()
+
+def login(email:str, password:str): # Try to login with user
+    c.execute("select 1 from users where email = '"+email+"' and password = '"+password+"';")
+    print("select 1 from users where email = '"+email+"' and password = '"+password+"';")
+    if c.fetchone == []: return 0
+    else : return getUserData(email)
+
+def getUserData(email:str):
+    c.execute("select * from tasks where userEmail = '"+email+"';")
+    return c.fetchall()
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import db;
+import db
+import tkinter
+import customtkinter
+import asyncio
 
 
 def exitApp():
    db.closeConnection()
 
-if __name__ == '__main__':
-   a = db.initDB()
-   exitApp()
+def initApplication():
+    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    customtkinter.set_default_color_theme("theme.json")
+    app.geometry("400x240")
 
+    txtUserName = customtkinter.CTkTextbox(master=app, width=100, height=10, corner_radius=0)
+    txtPassword = customtkinter.CTkTextbox(master=app, width=100, height=10, corner_radius=0)
+    btnLogin = customtkinter.CTkButton(app, text="Belépés", command=db.login(txtUserName.get("0.0", "end").replace('\n',""),txtPassword.get("0.0", "end").replace('\n',"")))
+
+    txtUserName.place(x=200, y=20, anchor=tkinter.CENTER)
+    txtPassword.place(x=200, y=60, anchor=tkinter.CENTER)
+    btnLogin.place(x=200, y=150, anchor=tkinter.CENTER)
+
+    app.mainloop()
+
+async def main():   
+   await db.initDB()
+   initApplication()
+
+asyncio.run(main())
